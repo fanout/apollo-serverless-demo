e23fe62b3ea82d44d6edfa6e439e029f1d38dcb8
Benjamin Goering <ben@bengo.co>
2019-04-29 14:44:28 -0400

    tslint

--- src/@types/killable/index.d.ts
+++ src/@types/killable/index.d.ts
@@ -1,6 +1,10 @@
 declare module "killable" {
   import { Server } from "http";
-  type KillableServerType = Server & { kill: (cb: Function) => void };
+  type KillableServerType = Server & {
+    /** kill the http server, closing all connections */
+    kill: (errback: (error?: Error) => void) => void
+  };
+  // tslint:disable-next-line:completed-docs
   function killable(server: Server): KillableServerType;
   namespace killable {
     export type KillableServer = KillableServerType;
